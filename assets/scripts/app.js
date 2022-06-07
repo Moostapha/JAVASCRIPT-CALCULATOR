34b155e9328732da3bbf23c74d835d828b1254b7
Moustapha Touré <moostoure@gmail.com>
2022-06-07 08:50:29 +0200

    commit objet logEntryPackage
    - Ecriture function logEntryPackage() pour add()
    - Factorisation du code pour cette fonction et réutilisation dans les autres fonctions opération
    - Factorisation du code avec utilisation de createAndWriteOutput pour les 3 autres opérations - */
    - logEntryPackage prend 4 arguments
    1) le type d'opération
    2) le résultat précédent
    3) le nombre entré par le user pour son opération
    4) le résultat final
    Ces 4 éléments constituent un objet qui est push dans le Array logEntries à chaque clique des boutons d'opération

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,74 +3,123 @@ const defaultResult = 0;
 // assignation d'une constante à une variable currentResult
 let currentResult = defaultResult; 
 
-// Variable globale pour array => Tableau vide qui grandira avec le temps
-let logEntries = [];
 
-// Objet global => package d'infos groupés ensemble donnant plusieurs infos variées de différents types
-
-
-// FONCTION PERMETTANT REFACTORING
+// FONCTIONS PERMETTANT REFACTORING
 
 // Fonction capturant le user input et le convertissant en int (car string par défaut)
 function getUserNumberInput(){
-    // userInput.value est de type string, il faut donc le convertir en int => parseInt() ou +
-    // utilisation de return nous permet de capter la valeur et de le use ailleurs
-    return parseInt(userInput.value)
+    // utilisation de return nous permet de capter la valeur et de le use ailleurs | userInput.value est de type string, il faut donc le convertir en int => parseInt() ou +
+    return parseInt(userInput.value); 
 }
 
 
 // Fonction donnat le détail du calcul de manière dynamique => Ordre des params est IMPORTANT !!!
 function createAndWriteOutput( resultBeforeCalculation, operator, newNumber ){
-    const calculDescription = `${resultBeforeCalculation} ${operator} ${newNumber}`;  // OUTPUT TEXT
-    outputResult(calculDescription, currentResult ); // vendor file
+    
+    // OUTPUT TEXT
+    const calculDescription = `${resultBeforeCalculation} ${operator} ${newNumber}`;
+    
+    // appel de la fonction outputResult de vendor.js avec les paramètres dynamiques
+    outputResult(calculDescription, currentResult ); 
+}
+
+// Variable globale pour array => Tableau vide qui grandira avec le temps
+let logEntries = [];
+
+
+// Objet global => package d'infos groupés ensemble donnant plusieurs infos variées de différents types
+
+function logEntryPackageInConsole(operation, previousResult, enteredNumber, currentResult){  // 4 params doivent match les valeurs de l'objet 
+    
+    // Objet package {clé: valeur} avec mutiples infos | datas groupés, structurés sous forme clé:valeur
+    const logEntryPackage = {                                                  
+        operation: operation,                 // values === params
+        previousResult: previousResult,       // Clés pointe vers une variable qui est le param. de la fonction de refactorisation
+        enteredNumber: enteredNumber,
+        result: currentResult
+    };
+    
+    // Ajout dans le array logEntries de l'objet logEntryPackage
+    logEntries.push(logEntryPackage); 
+    
+    // Affichage new objets dans le Array
+    console.log('Array des objets ajoutés par opérations', logEntries) 
 }
 
 
 // FONCTIONS DES 4 OPERATIONS
 
 function add(){
-    const enteredNumber = getUserNumberInput();                                // appel de la fonction getUserNumberInput et sa valeur donnée par return
-    const previousResult = currentResult;                                      // récupération de currentResult terme de droite avant ajout enteredNumber
-    console.log('previousResult addition:', previousResult);                   // Affichage dans console
-    currentResult = currentResult + enteredNumber;                             // addition => currentResult de gauche et de droite ne sont pas les sames valeurs
-    createAndWriteOutput( previousResult, '+', enteredNumber );                // Affichage résultat + détails calcul => 3 params résultat précédent auquel ajout nouvelle valeur
-    const logEntryPackage = {                                                  // Objet package {clé: valeur} avec mutiples infos
+    
+    // 1) appel de la fonction getUserNumberInput et sa valeur donnée par return
+    const enteredNumber = getUserNumberInput();  
+    
+    // 2) récupération de currentResult terme de droite avant ajout enteredNumber
+    const previousResult = currentResult;     
+    
+    // 3) Affichage dans console
+    console.log('previousResult addition:', previousResult);  
+    
+    // 4) addition => currentResult de gauche et de droite ne sont pas les sames valeurs
+    currentResult = currentResult + enteredNumber;
+    
+    // 5) Affichage résultat + détails calcul => 3 params résultat précédent auquel ajout nouvelle valeur
+    createAndWriteOutput( previousResult, '+', enteredNumber ); 
+    
+    // 7) Objet package {clé: valeur} avec multiples infos | datas groupés, structurés sous forme clé:valeur
+    const logEntryPackage = {                                                  
         operation: 'ADD',
-        previousResult: previousResult,
-        addedNumber: enteredNumber,
+        previousResult: previousResult,   
+        enteredNumber: enteredNumber,
         result: currentResult
     };
-    logEntries.push(logEntryPackage);                                          // Ajout dans le array logEntries de l'objet logEntryPackage
-    console.log('Array des objets ajoutés par add():', logEntries)             // Affichage Array des différents objets ajoutés
+    
+    // 8) Ajout dans le array logEntries de l'objet logEntryPackage
+    logEntries.push(logEntryPackage); 
+    
+    // 9) Affichage Array des différents objets ajoutés
+    console.log('Array des objets ajoutés par add():', logEntries);
+    
     // logEntries.push(enteredNumber);                                         // Ajout dans le tableau logEntries (vide de base cf ligne 7) de enteredNumber à chaque click
     // console.log('Array des valeurs ajoutées par add():', logEntries);       // Affichage des valeurs ajoutées par le user 
 }
 
 
-// fonction avant refactorisation
 function subtract(){
     const enteredNumber = getUserNumberInput();
-    // const calculDescription = `${currentResult} - ${enteredNumber}`;  => Refactorisation dans createAndWriteOutput
-    const previousResult = currentResult                                 // currentResult membre de droite
+    const previousResult = currentResult;
     console.log('previousResult soustraction:', previousResult); 
     currentResult = currentResult - enteredNumber;
-    // outputResult(calculDescription, currentResult );                  => Refactorisation dans createAndWriteOutput
+    // Appel fonction ligne 41 avec params adaptés pour -
     createAndWriteOutput(previousResult, '-', enteredNumber);
+    // Appel fonction ligne 14 avec params
+    logEntryPackageInConsole('SUBTRACT', previousResult , enteredNumber , currentResult);    // Refactorisation objets push dans Array logEntries cf fonction ligne 11 
 }
+
 
 // Ecriture des 2 autres fonctions sans la refactorisation faite avec createAndWriteOutput()
 function divide(){
     const enteredNumber = getUserNumberInput();
-    const calculDescription = `${currentResult} / ${enteredNumber}`;
+    const previousResult = currentResult;
+    console.log('previousResult division:', previousResult); 
     currentResult = currentResult / enteredNumber;
-    outputResult(calculDescription, currentResult);
+    // Appel fonction ligne 41 avec params adaptés
+    createAndWriteOutput(previousResult, '/', enteredNumber);
+    // Appel fonction ligne 14 avec params
+    logEntryPackageInConsole('DIVIDE', previousResult, enteredNumber, currentResult)
+
 }
+
 
 function multiply(){
     const enteredNumber = getUserNumberInput();
-    const calculDescription = `${currentResult} * ${enteredNumber}`;
+    const previousResult = currentResult;
+    console.log('previousResult multiplication:', previousResult); 
     currentResult = currentResult * enteredNumber;
-    outputResult(calculDescription , currentResult );
+    // Appel fonction ligne 41 avec params adaptés
+    createAndWriteOutput(previousResult, '*', enteredNumber);
+    // Appel fonction ligne 14 avec params
+    logEntryPackageInConsole('MULTIPLY', previousResult , enteredNumber , currentResult); 
 }
 
 
